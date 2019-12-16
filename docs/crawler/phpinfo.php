12ca1b46cf35753665a45a5c400950a243dfe143
MisterHK <abduhalilken@gmail.com>
2019-12-16 18:42:49 +0800

    V0.5.6-SNAPSHOT-V3

--- docs/crawler/phpinfo.php
+++ docs/crawler/phpinfo.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+// echo phpinfo();
+echo "wayback_machine_downloader http://en.miui.com/download.html --all-timestamps --concurrency 5 & wayback_machine_downloader http://www.miui.com/download.html  --all-timestamps --concurrency 5";
+echo "\n";
+for($i=1;$i<=400;$i++){
+    echo "wayback_machine_downloader http://en.miui.com/download-".$i.".html --all-timestamps --concurrency 5 & wayback_machine_downloader http://www.miui.com/download-".$i.".html  --all-timestamps --concurrency 5";
+    echo "\n";
+};
